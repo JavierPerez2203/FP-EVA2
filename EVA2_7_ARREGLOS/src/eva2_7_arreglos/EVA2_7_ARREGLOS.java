e89d440e843afe7fb4e7e858e488f150b037dbf2
JavierPerez2203 <L25550656@chihuahua2.tecnm.mx>
2025-10-06 21:32:49 -0700

    Add files via upload

---
+++ EVA2_7_ARREGLOS/src/eva2_7_arreglos/EVA2_7_ARREGLOS.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_7_arreglos;
+
+
+public class EVA2_7_ARREGLOS {
+
+
+    public static void main(String[] args) {
+       
+        String cade = "hola mundo!!";
+        System.out.println(cade.charAt(0));
+        char caracter = cade.charAt(11);
+        System.out.println(caracter);
+        System.out.println(cade.length());
+        int valor = 10;
+    }
+    
+}
