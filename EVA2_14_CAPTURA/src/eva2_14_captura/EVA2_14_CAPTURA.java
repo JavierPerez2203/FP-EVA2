287fbc02ddc5b25ebe4d63204cce278dc842d7de
JavierPerez2203 <L25550656@chihuahua2.tecnm.mx>
2025-10-08 15:50:01 -0700

    Add files via upload

---
+++ EVA2_14_CAPTURA/src/eva2_14_captura/EVA2_14_CAPTURA.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_14_captura;
+
+import java.util.Scanner;
+
+
+public class EVA2_14_CAPTURA {
+
+    
+    public static void main(String[] args) {
+       Scanner captu = new Scanner(System.in);
+       int tama;
+        System.out.println("cuantas calificaciones va a acpturar");
+        tama = captu.nextInt();
+        int califas[] = new int [tama];
+        for (int i = 0; i < califas.length; i++){
+            System.out.println("calificacion" + (i + 1) + ":");
+        tama = captu.nextInt();
+        }
+        for (int i = 0; i < califas.length; i++){
+            System.out.println("[" + califas[i] + "]");
+            
+        }
+    }
+    
+}
