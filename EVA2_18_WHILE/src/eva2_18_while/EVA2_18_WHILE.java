008fc5c80573bc7f833df11da14c9fb78d312aae
JavierPerez2203 <L25550656@chihuahua2.tecnm.mx>
2025-10-28 16:52:35 -0600

    Add files via upload

---
+++ EVA2_18_WHILE/src/eva2_18_while/EVA2_18_WHILE.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_18_while;
+
+
+public class EVA2_18_WHILE {
+
+    
+    public static void main(String[] args) {
+        int i = 1;
+        while (i<=10){// se repite si la condicion es verdad
+            System.out.println("HOLA");
+            i++;
+        }
+        //simular un for con do while
+        System.out.println("ciclo do while----");
+        i = 1;
+        do{
+          System.out.println("HOLA"); 
+          i++;
+        }while(i<=10);
+        
+    }
+    
+}
