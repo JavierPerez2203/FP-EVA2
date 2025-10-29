f471a1e2ef5b5a38e9216dfde893bca8396a4a27
JavierPerez2203 <L25550656@chihuahua2.tecnm.mx>
2025-10-29 16:48:30 -0600

    Add files via upload

---
+++ EVA2_20_CICLO_INFINITO/src/eva2_20_ciclo_infinito/EVA2_20_CICLO_INFINITO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_20_ciclo_infinito;
+
+
+public class EVA2_20_CICLO_INFINITO {
+
+   
+    public static void main(String[] args) {
+        
+        while(true){
+            System.out.println("JAVIER PEREZ");
+        }
+    }
+    
+}
